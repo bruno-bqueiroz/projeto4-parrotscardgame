fffe5038a731b7ce68f535123dd09a996d433868
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-06-20 16:35:24 -0400

    versão final

--- script.js
+++ script.js
@@ -24,40 +24,45 @@ function criarCartas(){
     }else {
         j=1;
     }
-    
     i = i+2;
     document.querySelector('.container').innerHTML += cartasTemplate;
 }
 criarCartas();
-
 }
-
-
-
 
 i = 0;
 
 const vira = document.querySelectorAll('.carta');
+
 
 let virarCarta = false;
 let bloqueio = false;
 let primeiraCarta = 0;
 let segundaCarta = 0;
 
+// adiciona a função flip pra que as cartas rotacionem
+
 function flipCard() {
-    if (bloqueio) return;   
+    if (bloqueio) return;   // bloqueia que o usuario vire uma terceira carta
+    
     this.classList.add('flip');
 
     if (!virarCarta){
         virarCarta = true;
         primeiraCarta = this;
+        
         return;
     }
+
     segundaCarta = this;
     virarCarta = false;
  
     verificarIgualdade();
+
   }
+
+
+  // verifica se a primeira carta é igual a segunda comparando o nome do arquivo
 
   function verificarIgualdade(){
     if (primeiraCarta.dataset.framework === segundaCarta.dataset.framework){
@@ -68,20 +73,31 @@ function flipCard() {
     desviraCarta();
   }
 
+  // se a primeira carta é igual a segunda desabilita a função click e ela perde os efeitos css
+
   function desabilitaVirada(){
       primeiraCarta.removeEventlistener('click', flipCard);
+      
       segundaCarta.removeEventlistener('click', flipCard);
+      
   }
 
-  function desviraCarta(){
-      bloqueio = true;
+// se a primeira carta é igual a segunda desabilita a função flip e ela fica estatica
+
+  function desviraCarta(){  
+      bloqueio = true;  // desbloqueia que o usuario vire uma terceira carta
       setTimeout (() => {
         primeiraCarta.classList.remove('flip');
-        segundaCarta.classList.remove('flip');
+        segundaCarta.classList.remove('flip');     
+ // bloqueia que o usuario vire uma terceira carta até que passe pela etapa de verificação
         bloqueio = false;
-      }, 1500);
+        }, 1000);
+
+        
     }
 
+
+// embaralha as cartas
     (function embaralhar(){
         vira.forEach(card=>{
             let aleatorio = Math.floor(Math.random()*ncartas);
@@ -89,5 +105,9 @@ function flipCard() {
         });
     })();
 
+   
+    vira.forEach(card => card.addEventListener('click', flipCard));
+    
 
-  vira.forEach(card => card.addEventListener('click', flipCard));
+
+ 
