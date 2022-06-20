015ca02af072580c114e150841dd6ba768b7b7c1
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-06-20 09:47:45 -0400

    cartas embaralhadas

--- script.js
+++ script.js
@@ -9,21 +9,21 @@ while (ncartas % 2 !== 0 || ncartas < 4 || ncartas > 14 ){
 alert ("Joga ai Xomano!");
 
 const index = 2*ncartas;
+const k = ncartas/2;
 
 while(i<index){
 function criarCartas(){
     let cartasTemplate = `
-    <div class="carta">
+    <div class="carta" data-framework="${j}">
         <img class="front-face" img src="./imagens/0.png" alt="Face da Carta">
         <img class="back-face" img src="./imagens/${j}.gif" alt="Verso da Carta">
     </div>
  `
-    if (j<7){
-        j++;
+    if (j<k){
+        j++;    
     }else {
         j=1;
     }
-    
     
     i = i+2;
     document.querySelector('.container').innerHTML += cartasTemplate;
@@ -34,28 +34,60 @@ criarCartas();
 
 
 
+
 i = 0;
-
-
-
 
 const vira = document.querySelectorAll('.carta');
 
-
 let virarCarta = false;
+let bloqueio = false;
 let primeiraCarta = 0;
 let segundaCarta = 0;
 
 function flipCard() {
-    this.classList.toggle('flip');
+    if (bloqueio) return;   
     this.classList.add('flip');
 
     if (!virarCarta){
         virarCarta = true;
         primeiraCarta = this;
-
+        return;
     }
+    segundaCarta = this;
+    virarCarta = false;
+ 
+    verificarIgualdade();
   }
-  
+
+  function verificarIgualdade(){
+    if (primeiraCarta.dataset.framework === segundaCarta.dataset.framework){
+        desabilitaVirada();
+        return;
+    }
+
+    desviraCarta();
+  }
+
+  function desabilitaVirada(){
+      primeiraCarta.removeEventlistener('click', flipCard);
+      segundaCarta.removeEventlistener('click', flipCard);
+  }
+
+  function desviraCarta(){
+      bloqueio = true;
+      setTimeout (() => {
+        primeiraCarta.classList.remove('flip');
+        segundaCarta.classList.remove('flip');
+        bloqueio = false;
+      }, 1500);
+    }
+
+    (function embaralhar(){
+        vira.forEach(card=>{
+            let aleatorio = Math.floor(Math.random()*ncartas);
+            card.style.order = aleatorio;
+        });
+    })();
+
 
   vira.forEach(card => card.addEventListener('click', flipCard));
